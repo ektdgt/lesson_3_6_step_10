cc2b68d14dc69cbd69749dbd7d1df0191bd1f608
KoTe <kt@ktkt.kt>
2025-03-27 21:10:00 +0400

    final changes added

--- test_items.py
+++ test_items.py
@@ -1,9 +1,10 @@
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 
-def test_checking_button_on_french(browser):
+
+def test_checking_button_is_present(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(5)
-    button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
-    text_on_button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket').text
-    assert text_on_button == 'Ajouter au panier'
+    time.sleep(30)
+    assert visibility_of_element_located(browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')), 'Button should be present'
