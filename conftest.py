9794acd3c4225a2f6ea94444d7ef18aa6d295bbd
KoTe <kt@ktkt.kt>
2025-03-27 20:13:09 +0400

    changes to conftest added

--- conftest.py
+++ conftest.py
@@ -1,11 +1,23 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",  # Значение по умолчанию, если параметр не указан
+        help="Language for the test (e.g. 'ru' or 'en')"
+    )
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    user_language = request.config.getoption("--language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
