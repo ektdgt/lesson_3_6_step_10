c582dc1c7f7f1f9e5169626eb6b4514a22d2eaea
KoTe <kt@ktkt.kt>
2025-03-29 16:34:33 +0400

    add final changes

--- test_items.py
+++ test_items.py
@@ -1,10 +1,16 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import visibility_of_element_located
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_checking_button_is_present(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(30)
-    assert visibility_of_element_located(browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')), 'Button should be present'
+    time.sleep(5)
+    button = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'btn-add-to-basket'))
+    )
+    assert button.is_displayed(), 'Button should be present'
+    # Следующий assert всегда будет приводить к ошибке, т.к. условие всегда ложно (False).
+    # assert False, "Не удалось найти кнопку 'ДОБАВИТЬ В КОРЗИНУ' в течение 10 секунд"
